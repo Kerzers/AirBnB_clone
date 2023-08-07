a18da548c8fe54e1ebe6c0615f8a0be5e2fdb8b9
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-08-07 10:29:27 +0100

    2nd commit

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        timeform = "%Y-%m-%dT%H:%M:%S.%f"
+        
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, timeform)
+                elif k == "__class__":
+                    continue
+                else:
+                    self.__dict__[k] = v
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = type(self).__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
