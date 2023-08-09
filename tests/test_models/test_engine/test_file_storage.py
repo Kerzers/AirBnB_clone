32eb65535efe4882fe691c1b0db5992f1a752a70
kerzers <ahra.ahlame@gmail.com>
2023-08-09 00:33:17 -0700

    Add test files

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module test_file_storage"""
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Tests class: FileStorage"""
+
+    def test_init(self):
+        pass
