ca4be025b46188038c05202b429bbb53b6367481
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-08-14 10:11:41 +0100

    req

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""this module defines the class: BaseModel"""
+"""this module defines the class: BaseModel
+"""
 from uuid import uuid4
 from datetime import datetime
 import models
@@ -35,13 +36,15 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        """It prints: [<class name>] (<self.id>) <self.__dict__>"""
+        """It prints: [<class name>] (<self.id>) <self.__dict__>
+        """
         classe_name = type(self).__name__
         return "[{}] ({}) {}".format(classe_name, self.id, self.__dict__)
 
     def save(self):
         """Updates the public instance attribute updated_at
-        with the current datetime"""
+        with the current datetime
+        """
 
         self.updated_at = datetime.now()
         models.storage.save()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,11 +21,13 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        """returns the dictionary __objects"""
+        """returns the dictionary __objects
+        """
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """sets in __objects the obj with key <obj class name>.id
+        """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
