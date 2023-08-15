91dbe5bceb7b6332dc2e8e76eedf75fa1b6dcca8
kerzers <ahra.ahlame@gmail.com>
2023-08-15 13:38:29 -0700

    add test console

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this module tests console.py"""
+import unittest
+import sys
+import models
+from console import HBNBCommand
+from models.engine.file_storage import FileStorage
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """tests Class: HBNBCommand"""
+    pass
